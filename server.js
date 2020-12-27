0927a37270f1de9733ae3402ba1d5d359ab2d4fa
ogneyar <ogneyar-ne@ya.ru>
2020-12-27 14:16:22 +0300

    ttt

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
-var	port = process.env.PORT || 8000;
+var	port = process.env.PORT || 80;
+var	host = process.env.HOST || "127.0.0.1";
 	
 express().use(express.static('dist'))
     .get('*', (req, res) => res.sendFile(__dirname + '/dist/index.html'))
-    .listen(port, () => console.log(`Starting server. Listening on ${ port }`));
+    .listen(port, host, () => console.log(`Starting server. Listening on ${ port }`));
