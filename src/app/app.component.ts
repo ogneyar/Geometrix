a33c135f1bde6e6e91b07759e595a807ab998bfe
ogneyar <ogneyar-ne@ya.ru>
2021-01-03 19:09:59 +0300

    hz

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'geometrix';
+  // showContacts(ev): void {
+  //   console.log(ev.target)
+  //   console.log(ev.target.value) //в данном случае undefined
+  //   console.log(ev.keyCode) //в данном случае undefined
+  // }
 }
--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,15 +1,69 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+// const N = require('./n');
+// import { N } from './n';
+// console.log(N);
+
+// "use strict";
+// const nodemailer = require('nodemailer');
+
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.scss']
 })
+
 export class LandingComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private route: ActivatedRoute) {
+    this.route.queryParams.subscribe((params: any) => {
+      if (params['name'] == 'test') {
+        console.log("params['name'] = test")
+      }
+    })
   }
+  
+  ngOnInit(): void {}
+
+
+  onClickForm(): void {
+    const doc = document
+    let name = doc.getElementById('name')
+    let phone = doc.getElementById('phone')
+    let email = doc.getElementById('email')
+    // if (name.value) console.log("name.value = ",name.value)
+    // if (phone.value) console.log("phone.value = ",phone.value)
+    // if (email.value) console.log("email.value = ",email.value)
+  }
+
+  async onClickSMTP(): Promise<void> {
+    // "use strict";
+    // const nodemailer = require('nodemailer');
+    
+    // let testEmailAccount = await nodemailer.createTestAccount();
+    
+    // let transporter = nodemailer.createTransport({
+    //   host: 'smtp.mail.ru',
+    //   port: 25,
+    //   secure: false,
+    //   auth: {
+    //     user: "ya13th",
+    //     pass: "*******"
+    //   }
+    // });
+    
+    // let result = await transporter.sendMail({
+    //   from: '"Node js" <nodejs@example.com>',
+    //   to: "user@example.com, user@example.com",
+    //   subject: "Message from Node js",
+    //   text: "This message was sent from Node js server.",
+    //   html: "This <i>message</i> was sent from <strong>Node js</strong> server."
+    // });
+    
+    // console.log(result);
+  }
+
 
 }
