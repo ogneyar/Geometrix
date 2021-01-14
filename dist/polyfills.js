b6475485e4f6225fdc69fa3470ef07df49fb51e6
ogneyar <ogneyar-ne@ya.ru>
2021-01-14 11:17:22 +0300

    tests

--- dist/polyfills.js
+++ dist/polyfills.js
@@ -7,7 +7,7 @@
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\VSCode\Geometrix\src\polyfills.ts */"hN/g");
+module.exports = __webpack_require__(/*! G:\VSCode\Geometrix\src\polyfills.ts */"hN/g");
 
 
 /***/ }),
